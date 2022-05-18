b3a7a1d33a6993b479c5698fec1bf68bcba91e5a
MinsuJeon <chso294@naver.com>
2022-05-18 18:02:24 +0900

    feat: Formula 구조체 생성 및 CalculatorItemQueue 구조체와의 직접 연관 관계 형성

---
+++ Calculator/Calculator/Model/Formula.swift
@@ -0,0 +1,14 @@
+//
+//  Formula.swift
+//  Calculator
+//
+//  Created by 전민수 on 2022/05/18.
+//
+struct Formula {
+    let operands: CalculatorItemQueue<Double>
+    let operators: CalculatorItemQueue<String>
+    
+    func result() -> Double {
+        return 0.0
+    }
+}

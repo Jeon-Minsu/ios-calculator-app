e9a9734bcbcfa8d89c13ad68f6f6e8b7f9d1cc6b
MinsuJeon <chso294@naver.com>
2022-05-18 16:52:02 +0900

    feat: String의 extension 추가

---
+++ Calculator/Calculator/Model/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  Calculator
+//
+//  Created by 전민수 on 2022/05/18.
+//
+extension String {
+    func split(with target: Character) -> [String] {
+        self.split(separator: target).map { String($0) }
+    }
+}
+
+
+

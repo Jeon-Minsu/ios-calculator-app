8c3bceb999e4a95de3b433598ebf2e96ebbfc8ac
MinsuJeon <chso294@naver.com>
2022-05-24 19:39:46 +0900

    refactor: Operator case의 rawValue를 스토리보드의 currentTitle가 일치하도록 수정

--- Calculator/Calculator/Model/Operator.swift
+++ Calculator/Calculator/Model/Operator.swift
@@ -7,8 +7,8 @@
 enum Operator: Character, CaseIterable, CalculateItem {
     case add = "+"
     case subtract = "-"
-    case divide = "/"
-    case multiply = "*"
+    case divide = "÷"
+    case multiply = "×"
     
     func calculate(lhs: Double, rhs: Double) throws -> Double {
         switch self {
